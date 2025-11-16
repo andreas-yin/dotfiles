a3a5e17d02c13a979e91bb7ae603a9cb46db6a83
Andreas Yin <andreas.yin@gmx.net>
2025-11-17 00:00:04 +0100

    install LazyExtras plugins, update battery warning threshold to 15%

--- battery/.local/bin/battery.sh
+++ battery/.local/bin/battery.sh
@@ -2,7 +2,7 @@
 BAT=$(ls /sys/class/power_supply | grep BAT | head -n 1)
 while true; do
   bat_lvl=$(cat /sys/class/power_supply/${BAT}/capacity)
-  if [ "$bat_lvl" -le 100 ]; then
+  if [ "$bat_lvl" -le 15 ]; then
     notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
     sleep 1200
   else
